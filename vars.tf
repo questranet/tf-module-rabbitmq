893739c71709d6c2eaeb45cc5911e91da843671c
root <root@ip-172-31-45-45.ec2.internal>
2023-12-26 20:36:21 +0000

    INIT

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "env" {}
+variable "component" {}
+variable "vpc_id" {}
+variable "vpc_cidr" {}
+variable "subnets" {}
+variable "kms_key_id" {}
+variable "rabbitmq_instance_type" {}
+variable "zone_id" {}
+variable "bastion_node_cidr" {}
